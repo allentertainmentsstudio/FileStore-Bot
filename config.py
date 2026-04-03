815aad2139529f102dceb79914d040472b8b3c7f
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-04-03 12:24:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,9 +41,9 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkshortify.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "18f426b1ec7d8209333c2dcf381932921647b6e4")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/hwdownload/3")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shrink.pe")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "372b23dabae021f93ca15408fc7a674ec4ec2ce3")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/log_channel_a/238")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 
 SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
